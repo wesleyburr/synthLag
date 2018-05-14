d2c0b1b0052419ea73295c006ae3d730e545b9f9
Wesley Burr <wesley.burr@gmail.com>
2018-05-14 01:13:39 -0400

    Correct code for sim4

--- 2-sim4.R
+++ 2-sim4.R
@@ -1,9 +1,10 @@
 #
 #  Simulation 4
 #  * independent WN(0, \sigma^2) base
-#  * two periodic component
-#  * equal \omega terms
-#  * \phi_1 and \phi_2 for predictor vary in discrete steps seq(-pi, pi, pi/6)
+#  * 10 periodic component, amplitudes varying from 0.5 to 3 (randomly)
+#  * equal \omega terms across the 10 periodic components
+#  * \phi terms vary randomly from [-pi, pi]
+#  * true 'relationship' estimated by using 0-phase offsets
 #
 library("ggplot2")
 library("reshape2")
@@ -16,72 +17,61 @@ source("findMSCsig.R")
 set.seed(67)
 
 N <- 1000
-M <- 100
+M <- 250
 tarr <- 1:N
-phi_var1 <- seq(-pi, pi, pi/6)
-phi_var2 <- seq(-pi, pi, pi/6)
-cors <- vector(length = M)
-lmod <- vector(length = M)
 
-# Estimate linear model, no modification
-comp <- function(X, Y, M) {
-  for(j in 1:M) {
-    wn1 <- rnorm(N, 0, 1)
-    wn2 <- rnorm(N, 0, 1)
-    x1 <- wn1 + 2 * cos(2 * pi * 1/4 * tarr + X) + 2 * cos(2 * pi * 1/6 * tarr + Y)
-    x2 <- wn2 + 2 * cos(2 * pi * 1/4 * tarr + 0.0) + 2 * cos(2 * pi * 1/6 * tarr + 0.0)
-    lmod[j] <- lm(x2 ~ x1)$coefficients[2]
+freqs <- 1 / sample(1:100, 10, replace = FALSE)
+amps <- sample(seq(0.5, 3.0, 0.1), 10)
+synth_cut <- 1e-10
+synth_sig = 0.90
+
+lmods0 <- lmods1 <- lmods2 <- lmods3 <- lmods_syn <- vector(length = M)
+
+# iterate through realizations
+for(j in 1:M) {
+  x1 <- x1_noPh <- rnorm(N, 0, 1)
+  x2 <- rnorm(N, 0, 1)
+  for(m in 1:length(freqs)) {
+    x1 <- x1 + amps[m] * cos(2 * pi * freqs[m] * tarr + runif(1, min = -pi, max = pi))
+    x1_noPh <- x1_noPh + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+    x2 <- x2 + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
   }
-  c(mean(lmod), sd(lmod))
+  true_lmod <- lm(x2 ~ x1_noPh)$coefficients[2]
+
+  # Integer-valued lags
+  lmods0[j] <- lm(x2 ~ x1)$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods1[j] <- lm(x2[-N] ~ x1[-1])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods2[j] <- lm(x2[-c(N, N-1)] ~ x1[-c(1, 2)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods3[j] <- lm(x2[-(N:(N-2))] ~ x1[-(1:3)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+
+  # Synthetic lag
+  x1s <- split_and_align(x2, x1, freq_cut = synth_cut,
+                         msc_sig = synth_sig, NW = 3, K = 5, 
+                         dT = 86400
+                        )
+  lmods_syn[j] <- lm(x2 ~ x1s)$coefficients[2] / true_lmod * 100
 }
 
-# Estimate linear model, synthetic lag
-comp_syn <- function(X, Y, M, synth_cut = 1e-10, synth_sig = 0.999) {
-  for(j in 1:M) {
-    wn1 <- rnorm(N, 0, 1)
-    wn2 <- rnorm(N, 0, 1)
-    x1 <- wn1 + 2 * cos(2 * pi * 1/4 * tarr + X) + 2 * cos(2 * pi * 1/6 * tarr + Y)
-    x2 <- wn2 + 2 * cos(2 * pi * 1/4 * tarr + 0.0) + 2 * cos(2 * pi * 1/6 * tarr + 0.0)
-    x1s <- split_and_align(x2, x1, freq_cut = synth_cut,
-                           msc_sig = synth_sig, NW = 3, K = 5, 
-                           dT = 86400
-                          )
-    lmod[j] <- lm(x2 ~ x1s)$coefficients[2]
-  }
-  c(mean(lmod), sd(lmod))
-}
+res <- matrix(c(quantile(lmods0, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods1, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods2, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods3, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods_syn, probs = c(0.025, 0.50, 0.975))),
+              byrow = TRUE, nrow = 5, ncol = 3)
+res <- as.data.frame(res)
+rownames(res) <- c("Lag-0", "Lag-1", "Lag-2", "Lag-3", "SynthLag")
+colnames(res) <- c("2.5%", "50%", "97.5%")
 
-res1 <- res2 <- res1s <- res2s <- matrix(data = NA, nrow = length(phi_var1), ncol = length(phi_var2))
-#
-#  This is not fast, it's brute-force. There's many ways to improve the speed, but it's
-#  a demonstration.
-#
-for(j in 1:length(phi_var1)) {
-  cat(".")
-  for(k in 1:length(phi_var2)) {
-    vals <- comp(phi_var1[j], phi_var2[k], M)
-    vals_syn <- comp_syn(phi_var1[j], phi_var2[k], M)
+# We actually have the simulation-by-simulation results as percentages, so take
+# the synthLag result by case, and subtract the other 3 from it, and report this as
+# additional columns
 
-    res1[j, k] <- vals[1]
-    res2[j, k] <- vals[2]
-    res1s[j, k] <- vals_syn[1]
-    res2s[j, k] <- vals_syn[2]
-  }
-}
-cat("\n")
+res2 <- res
+res2[1, ] <- quantile(lmods_syn - lmods0, probs = c(0.025, 0.50, 0.975))
+res2[2, ] <- quantile(lmods_syn - lmods1, probs = c(0.025, 0.50, 0.975))
+res2[3, ] <- quantile(lmods_syn - lmods2, probs = c(0.025, 0.50, 0.975))
+res2[4, ] <- quantile(lmods_syn - lmods3, probs = c(0.025, 0.50, 0.975))
+res2[5, ] <- c(NA, NA, NA)
 
-colnames(res1) <- colnames(res1s) <- phi_var1
-rownames(res1) <- rownames(res1s) <- phi_var2
-melted_lmod1 <- melt(res1)
-melted_lmod1s <- melt(res1s)
-
-melted_lmod1s_perc <- melted_lmod1s
-melted_lmod1s_perc$value <- melted_lmod1s_perc$value / 0.80
-
-q <- ggplot(data = melted_lmod1s_perc, aes(x=Var1, y=Var2, fill=value)) + 
-     geom_tile(aes(fill = value), colour = "white") +
-     scale_fill_gradient(low = "white", high = "steelblue") +
-     ylab(expression(phi[2])) +
-     xlab(expression(phi[1]))
 
 
---
+++ 4-simS4.R
@@ -0,0 +1,199 @@
+#
+#  Simulation for Online Supplement, Section \S 4
+#  * simulation demonstrating the result of changing the cutoff
+#  * use the same setup as Sim 4 (as numbered in code)
+#  * change N and prob cut-off
+#
+library("ggplot2")
+library("reshape2")
+library("multitaper")
+source("split_and_align.R")
+source("findPeaks.R")
+source("filterPeaksW.R")
+source("findMSCsig.R")
+
+set.seed(67)
+
+N <- 1000
+M <- 250
+tarr <- 1:N
+
+freqs <- 1 / sample(1:100, 10, replace = FALSE)
+amps <- sample(seq(0.5, 3.0, 0.1), 10)
+synth_cut <- 1e-10
+synth_sig = 0.90
+
+lmods0 <- lmods1 <- lmods2 <- lmods3 <- lmods_syn <- vector(length = M)
+
+# iterate through realizations
+for(j in 1:M) {
+  x1 <- x1_noPh <- rnorm(N, 0, 1)
+  x2 <- rnorm(N, 0, 1)
+  for(m in 1:length(freqs)) {
+    x1 <- x1 + amps[m] * cos(2 * pi * freqs[m] * tarr + runif(1, min = -pi, max = pi))
+    x1_noPh <- x1_noPh + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+    x2 <- x2 + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+  }
+  true_lmod <- lm(x2 ~ x1_noPh)$coefficients[2]
+
+  # Integer-valued lags
+  lmods0[j] <- lm(x2 ~ x1)$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods1[j] <- lm(x2[-N] ~ x1[-1])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods2[j] <- lm(x2[-c(N, N-1)] ~ x1[-c(1, 2)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods3[j] <- lm(x2[-(N:(N-2))] ~ x1[-(1:3)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+
+  # Synthetic lag
+  x1s <- split_and_align(x2, x1, freq_cut = synth_cut,
+                         msc_sig = synth_sig, NW = 3, K = 5,
+                         dT = 86400
+                        )
+  lmods_syn[j] <- lm(x2 ~ x1s)$coefficients[2] / true_lmod * 100
+}
+
+res <- matrix(c(quantile(lmods0, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods1, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods2, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods3, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods_syn, probs = c(0.025, 0.50, 0.975))),
+              byrow = TRUE, nrow = 5, ncol = 3)
+res <- as.data.frame(res)
+rownames(res) <- c("Lag-0", "Lag-1", "Lag-2", "Lag-3", "SynthLag")
+colnames(res) <- c("2.5%", "50%", "97.5%")
+
+# We actually have the simulation-by-simulation results as percentages, so take
+# the synthLag result by case, and subtract the other 3 from it, and report this as
+# additional columns
+
+res2 <- res
+res2[1, ] <- quantile(lmods_syn - lmods0, probs = c(0.025, 0.50, 0.975))
+res2[2, ] <- quantile(lmods_syn - lmods1, probs = c(0.025, 0.50, 0.975))
+res2[3, ] <- quantile(lmods_syn - lmods2, probs = c(0.025, 0.50, 0.975))
+res2[4, ] <- quantile(lmods_syn - lmods3, probs = c(0.025, 0.50, 0.975))
+res2[5, ] <- c(NA, NA, NA)
+
+library("xtable")
+res1_x <- xtable(res1)
+res2_x <- xtable(res2)
+
+print(res1_x)
+print(res2_x)
+
+#
+# change to 0.95
+#
+synth_sig = 0.95
+
+lmods0 <- lmods1 <- lmods2 <- lmods3 <- lmods_syn <- vector(length = M)
+
+# iterate through realizations
+for(j in 1:M) {
+  x1 <- x1_noPh <- rnorm(N, 0, 1)
+  x2 <- rnorm(N, 0, 1)
+  for(m in 1:length(freqs)) {
+    x1 <- x1 + amps[m] * cos(2 * pi * freqs[m] * tarr + runif(1, min = -pi, max = pi))
+    x1_noPh <- x1_noPh + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+    x2 <- x2 + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+  }
+  true_lmod <- lm(x2 ~ x1_noPh)$coefficients[2]
+
+  # Integer-valued lags
+  lmods0[j] <- lm(x2 ~ x1)$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods1[j] <- lm(x2[-N] ~ x1[-1])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods2[j] <- lm(x2[-c(N, N-1)] ~ x1[-c(1, 2)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods3[j] <- lm(x2[-(N:(N-2))] ~ x1[-(1:3)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+
+  # Synthetic lag
+  x1s <- split_and_align(x2, x1, freq_cut = synth_cut,
+                         msc_sig = synth_sig, NW = 3, K = 5,
+                         dT = 86400
+                        )
+  lmods_syn[j] <- lm(x2 ~ x1s)$coefficients[2] / true_lmod * 100
+}
+
+res <- matrix(c(quantile(lmods0, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods1, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods2, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods3, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods_syn, probs = c(0.025, 0.50, 0.975))),
+              byrow = TRUE, nrow = 5, ncol = 3)
+res <- as.data.frame(res)
+rownames(res) <- c("Lag-0", "Lag-1", "Lag-2", "Lag-3", "SynthLag")
+colnames(res) <- c("2.5%", "50%", "97.5%")
+
+# We actually have the simulation-by-simulation results as percentages, so take
+# the synthLag result by case, and subtract the other 3 from it, and report this as
+# additional columns
+
+res2 <- res
+res2[1, ] <- quantile(lmods_syn - lmods0, probs = c(0.025, 0.50, 0.975))
+res2[2, ] <- quantile(lmods_syn - lmods1, probs = c(0.025, 0.50, 0.975))
+res2[3, ] <- quantile(lmods_syn - lmods2, probs = c(0.025, 0.50, 0.975))
+res2[4, ] <- quantile(lmods_syn - lmods3, probs = c(0.025, 0.50, 0.975))
+res2[5, ] <- c(NA, NA, NA)
+
+res1_x <- xtable(res1)
+res2_x <- xtable(res2)
+
+print(res1_x)
+print(res2_x)
+
+#
+# change to 0.99
+#
+synth_sig = 0.99
+
+lmods0 <- lmods1 <- lmods2 <- lmods3 <- lmods_syn <- vector(length = M)
+
+# iterate through realizations
+for(j in 1:M) {
+  x1 <- x1_noPh <- rnorm(N, 0, 1)
+  x2 <- rnorm(N, 0, 1)
+  for(m in 1:length(freqs)) {
+    x1 <- x1 + amps[m] * cos(2 * pi * freqs[m] * tarr + runif(1, min = -pi, max = pi))
+    x1_noPh <- x1_noPh + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+    x2 <- x2 + amps[m] * cos(2 * pi * freqs[m] * tarr + 0.0)
+  }
+  true_lmod <- lm(x2 ~ x1_noPh)$coefficients[2]
+
+  # Integer-valued lags
+  lmods0[j] <- lm(x2 ~ x1)$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods1[j] <- lm(x2[-N] ~ x1[-1])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods2[j] <- lm(x2[-c(N, N-1)] ~ x1[-c(1, 2)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+  lmods3[j] <- lm(x2[-(N:(N-2))] ~ x1[-(1:3)])$coefficients[2] / true_lmod * 100   # record the percent of true, +/-
+
+  # Synthetic lag
+  x1s <- split_and_align(x2, x1, freq_cut = synth_cut,
+                         msc_sig = synth_sig, NW = 3, K = 5,
+                         dT = 86400
+                        )
+  lmods_syn[j] <- lm(x2 ~ x1s)$coefficients[2] / true_lmod * 100
+}
+
+res <- matrix(c(quantile(lmods0, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods1, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods2, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods3, probs = c(0.025, 0.50, 0.975)), 
+                quantile(lmods_syn, probs = c(0.025, 0.50, 0.975))),
+              byrow = TRUE, nrow = 5, ncol = 3)
+res <- as.data.frame(res)
+rownames(res) <- c("Lag-0", "Lag-1", "Lag-2", "Lag-3", "SynthLag")
+colnames(res) <- c("2.5%", "50%", "97.5%")
+
+# We actually have the simulation-by-simulation results as percentages, so take
+# the synthLag result by case, and subtract the other 3 from it, and report this as
+# additional columns
+
+res2 <- res
+res2[1, ] <- quantile(lmods_syn - lmods0, probs = c(0.025, 0.50, 0.975))
+res2[2, ] <- quantile(lmods_syn - lmods1, probs = c(0.025, 0.50, 0.975))
+res2[3, ] <- quantile(lmods_syn - lmods2, probs = c(0.025, 0.50, 0.975))
+res2[4, ] <- quantile(lmods_syn - lmods3, probs = c(0.025, 0.50, 0.975))
+res2[5, ] <- c(NA, NA, NA)
+
+res1_x <- xtable(res1)
+res2_x <- xtable(res2)
+
+print(res1_x)
+print(res2_x)
+
+
